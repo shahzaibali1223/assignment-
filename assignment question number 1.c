ff36a58454b688f1eb7b2537d188ea320364c853
shahzaibali1223 <shahzaibali14856@gmail.com>
2024-10-20 23:09:22 +0500

    Add files via upload

---
+++ assignment question number 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a, b, sum;
+	printf("entre a integer");
+	scanf("%d",&a);
+	printf("entre b integer");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("sum: %d\n",sum);
+	getch();
+	
+}
