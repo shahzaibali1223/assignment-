02e1b2c3c92d6d9e0baa48ba3f5b8d6e84273947
shahzaibali1223 <shahzaibali14856@gmail.com>
2024-10-20 23:12:39 +0500

    Add files via upload

---
+++ assignment question number 4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float fahrenheit, celsius;
+printf("entre temprature in fahrenheit");
+scanf("%f", &fahrenheit);
+celsius=(fahrenheit - 32)*5/9;
+printf("%.2f fahrenheit is equal to %.2f celsius\n", fahrenheit, celsius);
+getch();
+}
