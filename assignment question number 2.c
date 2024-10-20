8c278203e872083baa278b3c15eeb39dbe7a78cb
shahzaibali1223 <shahzaibali14856@gmail.com>
2024-10-20 23:12:00 +0500

    Add files via upload

---
+++ assignment question number 2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int variable ;
+	printf("entre any num for variable ");
+	scanf("%d",&variable),
+	printf("square is %d",variable*variable);
+	
+	getch();
+	
+	}
